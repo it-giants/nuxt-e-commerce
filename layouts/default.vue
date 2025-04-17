bb68d41f3f3b1358bc6b692efb1f4a235ce2a79c
aistanbouli <a.e.i9444@gmail.com>
2025-04-17 22:59:57 +0300

    Identified the colors and used app.vue to handle dark mode as centeralized point of logic

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,17 +1,5 @@
 <script setup>
 import Header from '~/components/Header.vue';
-
-// useHead({
-//   script: [{
-//     children: `
-//      const isDark = localStorage.getItem('isDark');
-//       if (isDark === 'true') {
-//         document.documentElement.classList.add('dark');
-//       } else {
-//         document.documentElement.classList.remove('dark');
-//       }
-//   `}]
-// })
 </script>
 
 <template>
